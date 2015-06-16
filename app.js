b0978d8c17b0cacabb429a040f435213ddc9e11d
Son Tran-Nguyen <esente@gmail.com>
2015-06-16 15:46:05 +0700

    Cache the map on /:map route

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.get( '/favicon.ico', function *() {
 app.get( '/locations/:map/:lng/:lat', cache, loader, cache, locator );
 app.get( '/locations/:map/:address', geocoder, cache, loader, cache, locator );
 app.get( '/locations/:map', geocoder, cache, loader, cache, locator );
-app.get( '/:map', cache, loader );
+app.get( '/:map', cache, loader, cache );
 
 // Start server.
 var port = process.env.PORT || 9876;
